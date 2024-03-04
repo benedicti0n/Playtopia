b0b52212be7940c68a63e6ef193ea065adc876d5
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-03-05 03:47:47 +0530

    routes to App.jsx

--- Frontend/Playtopia/src/App.jsx
+++ Frontend/Playtopia/src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+
+import Layout from './Layout.jsx'
+import Home from './components/Home.jsx'
+import Login from './components/Login.jsx'
+import SignUp from './components/SignUp.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element= {<Home/>}/>
+      <Route path='signUp' element= {<SignUp/>}/>
+      <Route path='login' element= {<Login/>}/>
+      {/* <Route path='forgot-password' element= {<ForgotPassword/>}/> */}
+    </Route>
+  )
+)
+
 
 export default function App() {
   return (
     <>
+    <RouterProvider router={router}/>
     </>
   )
 }
