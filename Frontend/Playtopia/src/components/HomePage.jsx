1e955cc327395f83d3a9e5fcc8bcdf2b49dd2cd7
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-03-06 14:49:20 +0530

    countdown component add

--- Frontend/Playtopia/src/components/HomePage.jsx
+++ Frontend/Playtopia/src/components/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import Home from './Home'
 import Events from './Events'
 import Footer from './Footer'
+import Countdown from './Countdown'
 
 function HomePage() {
  
@@ -9,6 +10,7 @@ function HomePage() {
   return (
     <div>
         <div><Home/></div>
+        <div><Countdown/></div>
         <div><Events/></div>
         <div><Footer></Footer></div>
     </div>
