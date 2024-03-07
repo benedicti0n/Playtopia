8174a170d0e7de5c60c52d8a2938f3c9403878fc
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-03-07 06:42:50 +0530

    loader component

--- Frontend/Playtopia/src/components/HomePage.jsx
+++ Frontend/Playtopia/src/components/HomePage.jsx
@@ -1,19 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Home from './Home'
 import Events from './Events'
 import Footer from './Footer'
 import Countdown from './Countdown'
+import Loader from './Loader'
+
+// import '../Styles/Transition.css'
 
 function HomePage() {
  
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 5000)
+
+  }, [])
   
   return (
+
     <div>
+      {
+        loading ?
+        <Loader/>
+        :
+        <div className=' transition'>
         <div><Home/></div>
         <div><Countdown/></div>
         <div><Events/></div>
         <div><Footer></Footer></div>
     </div>
+      }
+    </div>
+    
   )
 }
 
