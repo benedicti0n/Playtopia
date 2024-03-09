4d61cb3ebc6a24d7f047c53bc55157e9b7337736
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-03-09 17:43:07 +0530

    minor change

--- Frontend/Playtopia/src/components/HomePage.jsx
+++ Frontend/Playtopia/src/components/HomePage.jsx
@@ -20,7 +20,7 @@ function HomePage() {
 
 
   return (
-    <div className='h-screen snap-y snap-mandatory overflow-scroll z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[indigo]'>
+    <div className='h-screen snap-y snap-mandatory overflow-scroll scroll-smooth z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[indigo]'>
     <section  className='snap-center'>
       <Home/>
     </section>
